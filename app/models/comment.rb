e023895b7527dd9cc75ca56b826ba3071d3a97ac
Ameet <mehtaameet@gmail.com>
2012-04-18 18:38:49 -0500

    Initial commit

---
+++ app/models/comment.rb
@@ -0,0 +1,17 @@
+class Comment < ActiveRecord::Base
+  belongs_to :post
+end
+
+class CreateComments < ActiveRecord::Migration
+  def change
+    create_table :comments do |t|
+      t.string :commenter
+      t.text :body
+      t.references :post
+ 
+      t.timestamps
+    end
+ 
+    add_index :comments, :post_id
+  end
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Block::Application.config.secret_token = '679b5b590f6adac37f8549bb1c128bdd930276c9fbe79e5c92f8ef8c73bbb8d86c986136257b4f9cf5e6ebb4dcf27b22d80b217ba857241d328a767cf0248ef8'
